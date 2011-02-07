8d6fae47a123cf8484efd2e41e85341639562935
Lukasz Szpak <lukasz.j.szpak@gmail.com>
2011-02-07 19:43:47 +0100

    Pep 8 editorial updates.

--- movies.py
+++ movies.py
@@ -11,9 +11,10 @@ NOK = -1
 #               IMDB movie id and full title based on hash
 #PRECONDITIONS: internet connection must be available
 #               movie path must be valid
-#RETURN:        if movie path is OK and movie is found in 
-#               opensubtitles.org database then IMDB ID 
+#RETURN:        if movie path is OK and movie is found in
+#               opensubtitles.org database then IMDB ID
 #               movie title are returned
+
 
 class Server(object):
 
@@ -29,7 +30,8 @@ class Server(object):
     def init_connection(self):
         self.server = xmlrpclib.ServerProxy(self.API_address)
         logging.debug("Connected")
-        self.login = self.server.LogIn(self.user, self.password, self.country_code, self.ua)
+        self.login = self.server.LogIn(self.user, self.password,
+                                       self.country_code, self.ua)
         if self.OK_code in self.login['status']:
             self.token = self.login['token']
             logging.debug("Logged in")
@@ -57,50 +59,51 @@ class Server(object):
         else:
             logging.debug("Logout unsuccesful")
             return NOK
-        
- 
+
+
 class Movie(object):
 
     def __init__(self):
         self.IMDBID = 0
         self.movieTitle = ""
-    
-    def hash_file(self,path): 
-        try: 
-        
-            longlongformat = 'q'  # long long 
-            bytesize = struct.calcsize(longlongformat) 
-        
-            f = open(path, "rb") 
-        
-            filesize = os.path.getsize(path) 
-            hash = filesize 
-        
-            if filesize < 65536 * 2: 
-                return "SizeError" 
-                 
-            for x in range(65536/bytesize): 
-                buffer = f.read(bytesize) 
-                (l_value,)= struct.unpack(longlongformat, buffer)  
-                hash += l_value 
-                hash = hash & 0xFFFFFFFFFFFFFFFF #to remain as 64bit number  
-                         
-    
-            f.seek(max(0,filesize-65536),0) 
-            for x in range(65536/bytesize): 
-                buffer = f.read(bytesize) 
-                (l_value,)= struct.unpack(longlongformat, buffer)  
-                hash += l_value 
-                hash = hash & 0xFFFFFFFFFFFFFFFF 
-                 
-            f.close() 
-            returnedhash =  "%016x" % hash 
+
+    def hash_file(self, path):
+    #method imported directly from opensubtitles.org
+    #needed to calculate hash
+        try:
+
+            longlongformat = 'q'  # long long
+            bytesize = struct.calcsize(longlongformat)
+
+            f = open(path, "rb")
+
+            filesize = os.path.getsize(path)
+            hash = filesize
+
+            if filesize < 65536 * 2:
+                return "SizeError"
+
+            for x in range(65536 / bytesize):
+                buffer = f.read(bytesize)
+                (l_value,)= struct.unpack(longlongformat, buffer)
+                hash += l_value
+                hash = hash & 0xFFFFFFFFFFFFFFFF #to remain as 64bit number
+
+            f.seek(max(0, filesize - 65536), 0)
+            for x in range(65536 / bytesize):
+                buffer = f.read(bytesize)
+                (l_value,)= struct.unpack(longlongformat, buffer)
+                hash += l_value
+                hash = hash & 0xFFFFFFFFFFFFFFFF
+
+            f.close()
+            returnedhash = "%016x" % hash
             #store calculated hash
             logging.debug("Hash calculated")
             self.hash = returnedhash
             return OK
-    
-        except(IOError): 
+
+        except(IOError):
             logging.debug("Hash not calculated")
             return NOK
 
