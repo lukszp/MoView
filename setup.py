a0ef89440f01ec3d6d7db7a7570cf695f30bb675
Lukasz Szpak <lukasz.j.szpak@gmail.com>
2011-03-08 19:07:47 +0100

    setup.py first approach. problem with imdbpy packaging

---
+++ setup.py
@@ -0,0 +1,51 @@
+import os
+from setuptools import setup, find_packages
+
+# Utility function to read the README file.
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = "moView",
+    version = "0.0.1",
+    packages = find_packages(),
+
+    entry_points = {
+        'console_scripts': ['moview=moview.moview:main'],
+        },
+
+    install_requires=[
+        'jinja2',
+        'xmlrpclib',
+        'argparse',
+        'imdbpy',
+        ],
+
+    author = "Lukasz Szpak",
+    author_email = "lukasz.j.szpak@gmail.com",
+    description = ("MoView allows you to obtain most important movie data from "
+                   " imdb.com and choose what's worth watching tonight."),
+    license = "BSD",
+
+    keywords = "media film movie utility",
+
+    url = "https://github.com/lukszp/MoView",
+
+    long_description=read('README'),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: BSD License",
+    ],
+    data_files=[('templates', ['moview/templates/index.html']),
+                  ('static', ['moview/static/default.css']),
+                  ('static/images', ['moview/static/images/img01.gif', 
+                                     'moview/static/images/img02.jpg', 
+                                     'moview/static/images/img03.jpg',
+                                     'moview/static/images/img04.jpg',
+                                     'moview/static/images/img05.gif'])],
+
+)
