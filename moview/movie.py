e6f460a81b90de659d4650fd8c1b1944ef896437
Lukasz Szpak <lukasz.j.szpak@gmail.com>
2011-03-13 17:25:56 +0100

    Closes #19

--- moview/movie.py
+++ moview/movie.py
@@ -8,6 +8,7 @@ class Movie(object):
     imdb.com
     """
 
+
     def __init__(self):
         self.imdb_id = NO_IMDBID_FOUND
         self.path = None
@@ -19,19 +20,25 @@ class Movie(object):
         self.rating = None
         self.plot = None
 
+    def to_unicode_or_bust(self, obj, encoding='utf-8'):
+        if isinstance(obj, basestring):
+            if not isinstance(obj, unicode):
+                obj = unicode(obj, encoding)
+        return obj
+
     def prepare_data_from_imdb(self):
         keys = self.imdb_object.keys()
-        self.path = self.path.decode('utf-8')
+        self.path = self.to_unicode_or_bust(self.path)
         if 'title' in keys:
-            self.title = ''.join(self.imdb_object['title']).decode('utf-8')
+            self.title = self.to_unicode_or_bust(''.join(self.imdb_object['title']))
         if 'genres' in keys:
-            self.genres = ' '.join(self.imdb_object['genres']).decode('utf-8')
+            self.genres = self.to_unicode_or_bust(' '.join(self.imdb_object['genres']).decode('utf-8'))
         if 'runtimes' in keys:
-            self.runtime = ' '.join(self.imdb_object['runtime']).decode('utf-8')
+            self.runtime = self.to_unicode_or_bust(' '.join(self.imdb_object['runtime']).decode('utf-8'))
         if 'languages' in keys:
-            self.language = ' '.join(self.imdb_object['language']).decode('utf-8')
+            self.language = self.to_unicode_or_bust(' '.join(self.imdb_object['language']).decode('utf-8'))
         if 'rating' in keys:
             self.rating = self.imdb_object['rating']
         if 'plot' in keys:
-            self.plot = ' '.join(self.imdb_object['plot']).decode('utf-8')
+            self.plot = self.to_unicode_or_bust(' '.join(self.imdb_object['plot']).decode('utf-8'))
             self.plot = self.plot[:self.plot.find('::')]
